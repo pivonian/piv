4df62de4d5d4ae803bc886e9c3bb4174ab1da23c
pivonian <bodhireimer@gmail.com>
2025-09-18 11:48:01 +1000

    blakjack

---
+++ python projects/blackjack.py
@@ -0,0 +1,28 @@
+import random
+
+cardTotal = 0
+dealer = random.randint(1, 13)
+
+cardTotal = random.randint(1, 13) + random.randint(1, 13)
+while True:
+    if cardTotal >= 21:
+        print('Bust!')
+    else:
+        print(dealer)
+        que = input('hit or stand? ')
+        if que.lower() == 'stand':
+            dealer += random.randint(1, 13)
+            if dealer >= 21:
+                print('You win!')
+        elif que.lower() == 'hit':
+            cardTotal += random.randint(1, 13)
+            if cardTotal != 21:
+                print(cardTotal)
+                if cardTotal <= 21 and cardTotal >= dealer:
+                    print('You win!')
+                else:
+                    if dealer <= 21:
+                        dealer += random.randint(1, 13)
+                    continue
+    break
+input('Press [Enter] to close.')
